955a027bd4253b1538a0d1f192f5a8ae4bb6ff00
kumarcoder06 <itskumarnitesh42@gmail.com>
2025-08-15 05:27:46 +0530

    complete

--- zentrox/settings.py
+++ zentrox/settings.py
@@ -26,8 +26,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = []
-
+import os
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOST', '').split(',')
+if ALLOWED_HOSTS == ['*']:
+    ALLOWED_HOSTS = ['*'] 
 
 # Application definition
 
