7dc6eae52793d43224ae0ba2ecfaa6b856d8414e
kumarcoder06 <itskumarnitesh42@gmail.com>
2025-08-15 22:15:00 +0530

    complete

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
+pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+python manage.py migrate --no-input
+
+if [[ $CREATE_SUPERUSER == "true" ]]; then
+  python manage.py createsuperuser \
+    --no-input \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL"
 fi
--- zentrox/settings.py
+++ zentrox/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,7 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS=[
-    'zentrox/static',
+     BASE_DIR / 'zentrox' / 'static',
 ]
 
 
